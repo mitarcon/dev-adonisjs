101d2504c4512ad1ff7f10b9a61551e9fd004681
Mitchell Contreras <mitarcon@gmail.com>
2019-06-26 19:30:23 +0000

    Añadir controlador de usuario

---
+++ wallet-api/app/Controllers/Http/UserController.js
@@ -0,0 +1,90 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+
+/**
+ * Resourceful controller for interacting with users
+ */
+class UserController {
+  /**
+   * Show a list of all users.
+   * GET users
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async index ({ request, response }) {
+  }
+
+  /**
+   * Render a form to be used for creating a new user.
+   * GET users/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async create ({ request, response }) {
+  }
+
+  /**
+   * Create/save a new user.
+   * POST users
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single user.
+   * GET users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async show ({ request, response }) {
+  }
+
+  /**
+   * Render a form to update an existing user.
+   * GET users/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {Response} ctx.params
+   */
+  async edit ({ params, request, response }) {
+  }
+
+  /**
+   * Update user details.
+   * PUT or PATCH users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {Response} ctx.params
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a user with id.
+   * DELETE users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = UserController
